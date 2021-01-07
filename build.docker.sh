093c16798a2825f33d6cfae35e06063bea49cb1a
Peter Wu <piterwu@outlook.com>
2021-01-07 12:56:58 +0800

    更新

--- build.docker.sh
+++ build.docker.sh
@@ -2,4 +2,4 @@
 
 sudo docker build -f amd64/Dockerfile . -t bestwu/deepin -t bestwu/deepin:stable 
 sudo docker build -f i386/Dockerfile . -t bestwu/deepin:i386 -t bestwu/deepin:stable-i386 
-# sudo docker push bestwu/deepin
+# sudo docker push -a bestwu/deepin
